b93799f4138fc297e19596d86f93d63b7c79555b
CatnissCullen <chael_c@qq.com>
2024-12-23 14:06:28 +0800

    更改到本地fork目录

--- lib/layout_utils.py
+++ lib/layout_utils.py
@@ -18,7 +18,6 @@ import torch.nn as nn
 
 from layout_config import get_config
 
-
 ###########################################################
 ## Directory
 ###########################################################
@@ -59,7 +58,7 @@ def json_load(path):
 def json_save(path, data):
     with open(path, 'w') as fid:
         json.dump(data, fid, indent=4, sort_keys=True)
-        
+
 
 ###########################################################
 ## Discretization
@@ -68,36 +67,35 @@ def json_save(path, data):
 class CocoLocationMap(object):
     def __init__(self, config):
         self.cfg = config
-        self.margin = 0.001 # avoid numerical troubles
+        self.margin = 0.001  # avoid numerical troubles
         self.cols, self.col_step = \
-            np.linspace(self.margin, 1.0 - self.margin, num=config.grid_size[0], 
-                endpoint=True, retstep=True, dtype=np.float)
+            np.linspace(self.margin, 1.0 - self.margin, num=config.grid_size[0],
+                        endpoint=True, retstep=True, dtype=np.float64)
         self.rows, self.row_step = \
-            np.linspace(self.margin, 1.0 - self.margin, num=config.grid_size[1], 
-                endpoint=True, retstep=True, dtype=np.float)
+            np.linspace(self.margin, 1.0 - self.margin, num=config.grid_size[1],
+                        endpoint=True, retstep=True, dtype=np.float64)
         Xs, Ys = np.meshgrid(self.cols, self.rows)
         self.coords = np.vstack((Xs.flatten(), Ys.flatten())).transpose()
 
     def index2coord(self, index):
         return self.coords[index].copy()
-    
+
     def indices2coords(self, indices):
         return self.coords[indices].copy()
-        
-    def coord2index(self, coord):
-        
-        col_idx = int((coord[0] - self.margin)/self.col_step + 0.5)
-        row_idx = int((coord[1] - self.margin)/self.row_step + 0.5)
 
-        col_idx = max(0, min(col_idx, self.cfg.grid_size[0]-1))
-        row_idx = max(0, min(row_idx, self.cfg.grid_size[1]-1))
+    def coord2index(self, coord):
+        col_idx = int((coord[0] - self.margin) / self.col_step + 0.5)
+        row_idx = int((coord[1] - self.margin) / self.row_step + 0.5)
+
+        col_idx = max(0, min(col_idx, self.cfg.grid_size[0] - 1))
+        row_idx = max(0, min(row_idx, self.cfg.grid_size[1] - 1))
         return row_idx * self.cfg.grid_size[0] + col_idx
-    
+
     def coords2indices(self, coords):
-        grids = (coords - self.margin)/np.array([self.col_step, self.row_step]).reshape((1,2))
-        grids = (grids + 0.5).astype(np.int)
-        grids[:, 0] = np.maximum(0, np.minimum(grids[:, 0], self.cfg.grid_size[0]-1))
-        grids[:, 1] = np.maximum(0, np.minimum(grids[:, 1], self.cfg.grid_size[1]-1))
+        grids = (coords - self.margin) / np.array([self.col_step, self.row_step]).reshape((1, 2))
+        grids = (grids + 0.5).astype(np.uint8)
+        grids[:, 0] = np.maximum(0, np.minimum(grids[:, 0], self.cfg.grid_size[0] - 1))
+        grids[:, 1] = np.maximum(0, np.minimum(grids[:, 1], self.cfg.grid_size[1] - 1))
         return grids[:, 1] * self.cfg.grid_size[0] + grids[:, 0]
 
 
@@ -106,34 +104,34 @@ class CocoTransformationMap(object):
         self.cfg = config
         self.margin = 0.001
         self.scales, self.scale_step = \
-            np.linspace(self.margin, 1.0 - self.margin, num=config.num_scales, 
-                endpoint=True, retstep=True, dtype=np.float32)
+            np.linspace(self.margin, 1.0 - self.margin, num=config.num_scales,
+                        endpoint=True, retstep=True, dtype=np.float32)
         ratios = []
-        K = int((config.num_ratios - 1)/2)
+        K = int((config.num_ratios - 1) / 2)
         for i in range(K, 0, -1):
-            ratios.append(1.0/(1+i))
+            ratios.append(1.0 / (1 + i))
         for i in range(0, K + 1):
-            ratios.append(1+i)
+            ratios.append(1 + i)
         self.ratios = np.array(ratios).astype(np.float32)
 
     def index2coord(self, inds):
         return np.array([self.scales[inds[0]], self.ratios[inds[1]]])
-    
+
     def indices2coords(self, inds):
         scales = self.scales[inds[:, 0]]
         ratios = self.ratios[inds[:, 1]]
         coords = np.stack([scales, ratios], -1)
         return coords
-        
+
     def coord2index(self, coord):
-        scale_idx = int((coord[0] - self.margin)/self.scale_step + 0.5)
-        scale_idx = max(0, min(scale_idx, self.cfg.num_scales-1))
+        scale_idx = int((coord[0] - self.margin) / self.scale_step + 0.5)
+        scale_idx = max(0, min(scale_idx, self.cfg.num_scales - 1))
         ratio_idx = np.argmin(np.absolute(self.ratios - coord[1]))
         return np.array([scale_idx, ratio_idx])
-    
+
     def coords2indices(self, coords):
-        scale_indices = ((coords[:, 0] - self.margin)/self.scale_step + 0.5).astype(np.int)
-        scale_indices = np.maximum(0, np.minimum(scale_indices, self.cfg.num_scales-1))
+        scale_indices = ((coords[:, 0] - self.margin) / self.scale_step + 0.5).astype(np.uint8)
+        scale_indices = np.maximum(0, np.minimum(scale_indices, self.cfg.num_scales - 1))
         foo = coords[:, 1].reshape((-1, 1))
         bar = np.repeat(self.ratios[None, ...], foo.shape[0], axis=0)
         ratio_indices = np.argmin(np.absolute(foo - bar), axis=-1)
@@ -143,9 +141,9 @@ class CocoTransformationMap(object):
         scale = math.sqrt(wh[0] * wh[1] + self.cfg.eps)
         ratio = wh[0] / (wh[1] + self.cfg.eps)
         return np.array([scale, ratio])
-    
+
     def whs2coords(self, whs):
-        scales = np.sqrt(whs[:, 0] * whs[:, 1] + self.cfg.eps) 
+        scales = np.sqrt(whs[:, 0] * whs[:, 1] + self.cfg.eps)
         ratios = whs[:, 0] / (whs[:, 1] + self.cfg.eps)
         coords = np.stack([scales, ratios], axis=-1)
         return coords
@@ -153,34 +151,34 @@ class CocoTransformationMap(object):
     def coord2wh(self, coord):
         area = coord[0] * coord[0]
         w = math.sqrt(area * coord[1] + self.cfg.eps)
-        h = area/(w + self.cfg.eps)
+        h = area / (w + self.cfg.eps)
         wh = np.array([w, h])
         wh = np.maximum(0, np.minimum(wh, 1.0))
-        return wh 
-    
+        return wh
+
     def coords2whs(self, coords):
         areas = coords[:, 0] * coords[:, 0]
         ws = np.sqrt(areas * coords[:, 1] + self.cfg.eps)
-        hs = areas/(ws + self.cfg.eps) 
+        hs = areas / (ws + self.cfg.eps)
         whs = np.stack([ws, hs], axis=-1)
         whs = np.maximum(0, np.minimum(whs, 1.0))
-        return whs 
+        return whs
 
     def index2wh(self, index):
         coord = self.index2coord(index)
         wh = self.coord2wh(coord)
         return wh
-    
+
     def indices2whs(self, indices):
         coords = self.indices2coords(indices)
         whs = self.coords2whs(coords)
         return whs
-    
+
     def wh2index(self, wh):
         coord = self.wh2coord(wh)
         index = self.coord2index(coord)
         return index
-    
+
     def whs2indices(self, whs):
         coords = self.whs2coords(whs)
         indices = self.coords2indices(coords)
@@ -191,8 +189,11 @@ class CocoTransformationMap(object):
 ## Vocabulary 
 ###########################################################
 import string
+
 punctuation_table = str.maketrans('', '', string.punctuation)
 stop_words = set(stopwords.words('english'))
+
+
 # print('stop_words: ', stop_words)
 
 def further_token_process(tokens):
@@ -212,7 +213,7 @@ def natural_keys(text):
     http://nedbatchelder.com/blog/200712/human_sorting.html
     (See Toothy's implementation in the comments)
     '''
-    return [ atoi(c) for c in re.split('(\d+)', text) ]
+    return [atoi(c) for c in re.split('(\d+)', text)]
 
 
 class Vocab(object):
@@ -225,7 +226,7 @@ class Vocab(object):
             self.word2index[word] = idx
             self.index2word.append(word)
             self.word2count[word] = 1
-        self.n_words = 3  
+        self.n_words = 3
 
         self.glovec = torchtext.vocab.GloVe(cache=osp.join(this_dir, '..', 'data', 'caches'))
 
@@ -276,7 +277,9 @@ class Vocab(object):
     def filter_words(self, max_size=None, min_freq=1):
         counter = Counter(self.word2count)
         # rm special tokens before sorting
-        counter['<pad>'] = 0; counter['<sos>'] = 0; counter['<eos>'] = 0
+        counter['<pad>'] = 0;
+        counter['<sos>'] = 0;
+        counter['<eos>'] = 0
         # sort by frequency, then alphabetically
         words_and_frequencies = sorted(counter.items(), key=lambda tup: tup[0])
         words_and_frequencies.sort(key=lambda tup: tup[1], reverse=True)
@@ -289,7 +292,7 @@ class Vocab(object):
             self.word2index[word] = idx
             self.index2word.append(word)
             self.n_words += 1
-        
+
         for word, freq in words_and_frequencies:
             if freq < min_freq or len(self.index2word) == max_size:
                 break
@@ -297,14 +300,18 @@ class Vocab(object):
             self.word2index[word] = self.n_words
             self.n_words += 1
 
-        counter['<pad>'] = 1; counter['<sos>'] = 1; counter['<eos>'] = 1
+        counter['<pad>'] = 1;
+        counter['<sos>'] = 1;
+        counter['<eos>'] = 1
         self.word2count = dict(counter)
 
     def word_to_index(self, w):
-        return self.word2index.get(w, -1)    
+        return self.word2index.get(w, -1)
 
-###########################################################
-## Pytorch 
+    ###########################################################
+
+
+## Pytorch
 ###########################################################
 
 class Flatten(nn.Module):
@@ -319,20 +326,20 @@ def conv3x3(in_planes, out_planes, stride=1):
 
 
 def weights_init(m):
-    classname = m.__class__.__name__     
+    classname = m.__class__.__name__
     if classname.find('Conv') != -1:
-        m.weight.data.normal_(0.0, 0.02)    
+        m.weight.data.normal_(0.0, 0.02)
     elif classname.find('BatchNorm') != -1:
         m.weight.data.normal_(1.0, 0.02)
         m.bias.data.fill_(0)
 
 
 def get_n_params(model):
-    pp=0
+    pp = 0
     for p in list(model.parameters()):
-        nn=1
+        nn = 1
         for s in list(p.size()):
-            nn = nn*s
+            nn = nn * s
         pp += nn
     return pp
 
@@ -353,7 +360,7 @@ def indices2onehots(indices, out_dim):
 class volume_normalize(object):
 
     def __init__(self, field):
-        self.field = field 
+        self.field = field
 
     def __call__(self, sample):
         raws = sample[self.field]
@@ -379,10 +386,10 @@ def vol_to_tensor(input_vol):
 ###########################################################
 def surface_to_image(surface):
     # get numpy data from cairo surface
-    pimg = PIL.Image.frombuffer("RGBA", 
-        (surface.get_width(), surface.get_height()),
-        surface.get_data(), "raw", "RGBA", 0, 1)
-    frame = np.array(pimg)[:,:,:-1]
+    pimg = PIL.Image.frombuffer("RGBA",
+                                (surface.get_width(), surface.get_height()),
+                                surface.get_data(), "raw", "RGBA", 0, 1)
+    frame = np.array(pimg)[:, :, :-1]
     return frame
 
 
@@ -395,21 +402,23 @@ def bb_iou(A, B):
     eps = 1e-8
     A_area = float(A[2] - A[0]) * (A[3] - A[1])
     B_area = float(B[2] - B[0]) * (B[3] - B[1])
-    minx = max(A[0], B[0]); miny = max(A[1], B[1])
-    maxx = min(A[2], B[2]); maxy = min(A[3], B[3])
+    minx = max(A[0], B[0]);
+    miny = max(A[1], B[1])
+    maxx = min(A[2], B[2]);
+    maxy = min(A[3], B[3])
     w = max(0, maxx - minx)
     h = max(0, maxy - miny)
     I_area = w * h
-    return I_area/(A_area + B_area - I_area + eps)
+    return I_area / (A_area + B_area - I_area + eps)
 
 
 def gaussian2d(x, y, sigmas):
-    v = (x - y)/np.array(sigmas)
+    v = (x - y) / np.array(sigmas)
     return np.exp(-0.5 * np.sum(v * v))
 
 
 def batch_gaussian1d(x, y, sigma):
-    v = (x - y)/sigma
+    v = (x - y) / sigma
     return np.exp(-0.5 * np.sum(v * v, -1))
 
 
@@ -420,10 +429,10 @@ def batch_gaussian1d(x, y, sigma):
 def clip_xyxy(box, width, height):
     box[0] = max(0, box[0])
     box[1] = max(0, box[1])
-    box[2] = min(box[2], width-1)
-    box[3] = min(box[3], height-1)
+    box[2] = min(box[2], width - 1)
+    box[3] = min(box[3], height - 1)
     return box.astype(np.int32)
-    
+
 
 def clip_xyxys(boxes, width, height):
     boxes[:, 0] = np.maximum(boxes[:, 0], 0)
@@ -434,13 +443,15 @@ def clip_xyxys(boxes, width, height):
 
 
 def xywh_to_xyxy(box, width, height):
-    x = box[0]; y = box[1]
-    w = box[2]; h = box[3]
+    x = box[0];
+    y = box[1]
+    w = box[2];
+    h = box[3]
 
     xmin = x - 0.5 * w + 1
-    xmax = x + 0.5 * w 
+    xmax = x + 0.5 * w
     ymin = y - 0.5 * h + 1
-    ymax = y + 0.5 * h 
+    ymax = y + 0.5 * h
 
     xyxy = np.array([xmin, ymin, xmax, ymax])
 
@@ -448,12 +459,14 @@ def xywh_to_xyxy(box, width, height):
 
 
 def xywhs_to_xyxys(boxes, width, height):
-    x = boxes[:, 0]; y = boxes[:, 1]
-    w = boxes[:, 2]; h = boxes[:, 3] 
+    x = boxes[:, 0];
+    y = boxes[:, 1]
+    w = boxes[:, 2];
+    h = boxes[:, 3]
 
     xmin = x - 0.5 * w + 1.0
     xmax = x + 0.5 * w
-    ymin = y - 0.5 * h + 1.0 
+    ymin = y - 0.5 * h + 1.0
     ymax = y + 0.5 * h
     xyxy = np.vstack((xmin, ymin, xmax, ymax)).transpose()
 
@@ -461,8 +474,10 @@ def xywhs_to_xyxys(boxes, width, height):
 
 
 def normalized_xywhs_to_xyxys(boxes):
-    x = boxes[:, 0]; y = boxes[:, 1]
-    w = boxes[:, 2]; h = boxes[:, 3] 
+    x = boxes[:, 0];
+    y = boxes[:, 1]
+    w = boxes[:, 2];
+    h = boxes[:, 3]
 
     xmin = x - 0.5 * w
     xmax = x + 0.5 * w
@@ -478,8 +493,7 @@ def normalized_xywhs_to_xyxys(boxes):
     return xyxy
 
 
-def xyxy_to_xywh(box):
-
+def xyxy_to_xywh(boxes):
     x = 0.5 * (boxes[0] + boxes[2])
     y = 0.5 * (boxes[1] + boxes[3])
     w = boxes[2] - boxes[0] + 1.0
@@ -489,7 +503,6 @@ def xyxy_to_xywh(box):
 
 
 def xyxys_to_xywhs(boxes):
-    
     x = 0.5 * (boxes[:, 0] + boxes[:, 2])
     y = 0.5 * (boxes[:, 1] + boxes[:, 3])
     w = boxes[:, 2] - boxes[:, 0] + 1.0
@@ -503,29 +516,30 @@ def xyxys_to_xywhs(boxes):
 ###########################################################
 
 def paint_box(ctx, color, box):
-    
-    x = box[0]; y = box[1]
+    x = box[0];
+    y = box[1]
     w = box[2] - box[0] + 1
     h = box[3] - box[1] + 1
-    
+
     ctx.set_source_rgb(color[0], color[1], color[2])
     ctx.set_line_width(10)
     ctx.rectangle(x, y, w, h)
     ctx.stroke()
-    
+
     # ctx.set_operator(cairo.OPERATOR_ADD)
     # ctx.fill()
-        
+
 
 def paint_txt(ctx, txt, box):
     font_option = cairo.FontOptions()
     font_option.set_antialias(cairo.Antialias.SUBPIXEL)
     ctx.set_source_rgb(0, 0, 0)
-    ctx.set_font_options(font_option) 
+    ctx.set_font_options(font_option)
     ctx.select_font_face("Purisa", cairo.FONT_SLANT_ITALIC, cairo.FONT_WEIGHT_BOLD)
     ctx.set_font_size(60)
     # ctx.set_operator(cairo.OPERATOR_ADD)
-    x = box[0]; y = box[1] + 50
+    x = box[0];
+    y = box[1] + 50
     w = box[2] - box[0] + 1
     h = box[3] - box[1] + 1
 
@@ -535,28 +549,28 @@ def paint_txt(ctx, txt, box):
 
 def create_squared_image(img, pad_value=None):
     if pad_value is None:
-        pad_value = np.array([255,255,255])
+        pad_value = np.array([255, 255, 255])
 
-    width  = img.shape[1]
+    width = img.shape[1]
     height = img.shape[0]
 
-    max_dim  = np.maximum(width, height)
-    offset_x = 0 #int(0.5 * (max_dim - width))
-    offset_y = max_dim - height #int(0.5 * (max_dim - height))
+    max_dim = np.maximum(width, height)
+    offset_x = 0  # int(0.5 * (max_dim - width))
+    offset_y = max_dim - height  # int(0.5 * (max_dim - height))
 
     output_img = pad_value.reshape(1, 1, img.shape[-1]) * \
                  np.ones((max_dim, max_dim, img.shape[-1]))
-    output_img[offset_y : offset_y + height, \
-               offset_x : offset_x + width,  :] = img
+    output_img[offset_y: offset_y + height, \
+    offset_x: offset_x + width, :] = img
 
     return output_img.astype(np.uint8), offset_x, offset_y
 
 
 def create_colormap(num_colors):
-    dz = np.arange(1, num_colors+1)
+    dz = np.arange(1, num_colors + 1)
     norm = plt.Normalize()
     colors = plt.cm.jet(norm(dz))
-    return colors[:,:3]
+    return colors[:, :3]
 
 
 ###########################################################
@@ -570,7 +584,7 @@ def log_coco_scores(infos, path):
     log_info['unigram_P'] = [np.mean(unigram_P), np.std(unigram_P), np.amin(unigram_P), np.amax(unigram_P)]
     unigram_R = infos.unigram_R()
     log_info['unigram_R'] = [np.mean(unigram_R), np.std(unigram_R), np.amin(unigram_R), np.amax(unigram_R)]
-    
+
     bigram_P = infos.mean_bigram_P()
     log_info['bigram_P'] = bigram_P
     bigram_R = infos.mean_bigram_R()
@@ -581,10 +595,12 @@ def log_coco_scores(infos, path):
     ratio = infos.ratio()
     log_info['ratio'] = [np.mean(ratio), np.std(ratio), np.amin(ratio), np.amax(ratio)]
     unigram_coord = infos.unigram_coord()
-    log_info['unigram_coord'] = [np.mean(unigram_coord), np.std(unigram_coord), np.amin(unigram_coord), np.amax(unigram_coord)]
+    log_info['unigram_coord'] = [np.mean(unigram_coord), np.std(unigram_coord), np.amin(unigram_coord),
+                                 np.amax(unigram_coord)]
     bigram_coord = infos.bigram_coord()
-    log_info['bigram_coord'] = [np.mean(bigram_coord), np.std(bigram_coord), np.amin(bigram_coord), np.amax(bigram_coord)]
-    
+    log_info['bigram_coord'] = [np.mean(bigram_coord), np.std(bigram_coord), np.amin(bigram_coord),
+                                np.amax(bigram_coord)]
+
     for k, v in log_info.items():
         print(k, v)
 
@@ -607,4 +623,3 @@ def layout_arguments(config):
     config.scale_loss_weight = 1.0
     config.ratio_loss_weight = 1.0
     return config
-    
